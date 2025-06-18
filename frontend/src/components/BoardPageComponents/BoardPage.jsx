f27ccb5a128d84c66d10dc81884e9c72e7972bda
Thomas Trivino <th992984@ucf.edu>
2025-06-18 11:38:56 -0700

    Adds header, body, and footer sections to BoardPage

--- frontend/src/components/BoardPageComponents/BoardPage.jsx
+++ frontend/src/components/BoardPageComponents/BoardPage.jsx
@@ -1,6 +1,33 @@
+import { boards, cards } from '../../../../backend/data' // Simulates API fetch
+import { useParams } from 'react-router'
+
+
 function BoardPage() {
-    return(
-        <></>
+    const { id } = useParams();
+    const idAsInt = parseInt(id)
+    const board = boards[id];
+    const cardBelongingToBoard = Object.values(cards).filter((card) => {
+        if (card.boardId === idAsInt)
+            return true;
+        return false;
+    });
+
+    return (
+        <section className="BoardPage-view">
+            <header className="BoardPage-banner">
+                <h1>KUDOBOARD</h1>
+                <h2>{board.title}</h2>
+
+            </header>
+
+            <section className="BoardPage-body">
+
+            </section>
+
+            <footer className="BoardPage-footer">
+                Thomas Trivino // Codepath 25
+            </footer>
+        </section>
     );
 }
 
