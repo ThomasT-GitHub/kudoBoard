89dc12c4beedd3f1ca041a1e84aed5f9e6279888
Thomas Trivino <th992984@ucf.edu>
2025-06-20 14:08:57 -0700

    Adds generated output to prisma

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('../generated/prisma');
 const prisma = new PrismaClient();
 const express = require('express');
 const cors = require('cors');
