ef0bc10c73ffb9a90d04093692de6be6eaf97000
Thomas Trivino <th992984@ucf.edu>
2025-06-20 11:43:05 -0700

    Adds CreateBoardButton components that handles creation of boards

--- frontend/src/components/HomePageComponents/CreateBoardButton.jsx
+++ frontend/src/components/HomePageComponents/CreateBoardButton.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import { createBoard } from "../../../utils/utils";
+import './HomePageComponentStyles/CreateBoardButton.css'
 
 function CreateBoardButton({ setBoards }) {
-    const handleBoardCreation = async (title, category, author) => {
-        const createdBoard = await createBoard("hi", "thank you", "dsa");
-        console.log(createdBoard)
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [author, setAuthor] = useState("");
+    const [modalShowing, setModalShowing] = useState(false);
+
+    const handleBoardCreation = async () => {
+        // Update the board remotely
+        const createdBoard = await createBoard(title, category, author);
+
+        // Update the board locally
         setBoards((previousBoards) => [...previousBoards, createdBoard]);
+    }
+
+    const showModal = () => {
+        setModalShowing(!modalShowing);
     }
 
     return (
         <>
-            <button className="create-board-button" onClick={handleBoardCreation}>Create Board</button>
+            <button className="CreateBoardButton-show-modal-button" onClick={showModal}>Create Board</button>
+            {modalShowing && <section className="CreateBoardButton-modal">
+                <form className="CreateBoardButton-form" onSubmit={handleBoardCreation}>
+                <label>
+                    Title:
+                    <input type="text" name="title" onChange={(e) => setTitle(e.target.value)} />
+                </label>
+                <label>
+                    Category:
+                    <input type="text" name="category" onChange={(e) => setCategory(e.target.value)} />
+                </label>
+                <label>
+                    Author (OPTIONAL):
+                    <input type="text" name="author" onChange={(e) => setAuthor(e.target.value)} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            </section>}
+
         </>
     );
 }
